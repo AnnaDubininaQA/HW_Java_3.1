e2fd8ce2d605e358e380a93bcc7f3bd18a593d5a
Anna test <annadubin@yandex.ru>
2022-12-11 10:13:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 10_000;
+        int percent = 20;
+        int milli = amount / percent;
+
+        System.out.println("Начисленные милли:" + milli);
+    }
+}
